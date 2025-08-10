9b7b5740181190347c947d41f9733fc362e25058
Mutlu Kurt <81134748+mutlukurt@users.noreply.github.com>
2025-08-10 16:04:27 +0300

    deploy: static export (fix worktree)

--- components/Skeleton.tsx
+++
@@ -1,9 +0,0 @@
-import { cn } from "@/lib/utils";
-
-export function Skeleton({ className }: { className?: string }) {
-  return (
-    <div className={cn("animate-pulse rounded-md bg-black/5 dark:bg-white/10", className)} />
-  );
-}
-
-
